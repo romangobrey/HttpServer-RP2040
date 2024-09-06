d8d2d0aa6985b137eb06d2062b3806394f62d5bb
Roman Gobrei <roman.gobrei@gmail.com>
2024-09-06 22:47:10 +0300

    config using ini file

--- configuration/W5500EvbPico.h
+++
@@ -1,5 +0,0 @@
-#include "Configuration.h"
-#include "DeviceModel.h"
-
-Rp2040::DeviceModel Config_DeviceModel = Rp2040::DeviceModel::W5500EvbPico;
-unsigned char Config_ServerIp[4] = {192, 168, 1, 170};
